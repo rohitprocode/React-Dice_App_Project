35d6eeb17d56926c45a34b56340b4b4320000c7b
rohitprocode <rohit990797@gmail.com>
2023-09-21 18:51:59 +0530

    Heading and Buttom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import {Flex} from "@chakra-ui/layout"
-import { Image } from "@chakra-ui/react";
+import {Flex,Heading,Stack} from "@chakra-ui/layout"
+import { Image } from "@chakra-ui/image";
+import { Button } from "@chakra-ui/button";
 
 const App = () =>{
   return (<Flex>
     <Image src="/dices.png"/>
+    <Stack>
+      <Heading as="h1">The Dice Game</Heading>
+      <Button>Play Now</Button>      
+    </Stack>
   </Flex>
   );
 }
