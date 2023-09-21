72208fff2d15a054a99eba7bcdcb50501d87881d
rohitprocode <rohit990797@gmail.com>
2023-09-21 19:29:03 +0530

    Button event

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { Flex, Heading, Stack } from "@chakra-ui/layout"
+import { Box,Flex, Heading, Stack } from "@chakra-ui/layout"
 import { Image } from "@chakra-ui/image";
 import { Button } from "@chakra-ui/button";
 import { useState } from "react";
 
 
 const App = () => {
-  const [gameStarted,setGameStarted] = useState(true); //useStateHook
+  const [gameStarted,setGameStarted] = useState(false); //useStateHook
+
+  const StartGameHandler = ()=>{
+    setGameStarted(true);
+    
+  }
   return <> (
-    {gameStarted &&(
+    {gameStarted ? <Box>Hii</Box> : (
       <Flex justify="center" align="center" >
       <Image width="50%" src="/dices.png" />
       <Stack>
         <Heading fontSize="7xl" as="h1">The Dice Game</Heading>
         <Button
+        onClick={StartGameHandler}
           alignSelf="flex-end"
           bg={"black"}
           color={"white"}
