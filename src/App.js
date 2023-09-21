5e6d8697abb6c5eb31d01f30b1a40ecdeb4b7cc8
rohitprocode <rohit990797@gmail.com>
2023-09-21 19:09:42 +0530

    Button Color

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import {Flex,Heading,Stack} from "@chakra-ui/layout"
+import { Flex, Heading, Stack } from "@chakra-ui/layout"
 import { Image } from "@chakra-ui/image";
 import { Button } from "@chakra-ui/button";
 
-const App = () =>{
+
+const App = () => {
   return (
-  <Flex justify="center" align="center" >
-    <Image width="50%" src="/dices.png"/>
-    <Stack>
-      <Heading fontSize="7xl" as="h1">The Dice Game</Heading>
-      <Button alignSelf="flex-end" >Play Now</Button>      
-    </Stack>
-  </Flex>
+    <Flex justify="center" align="center" >
+      <Image width="50%" src="/dices.png" />
+      <Stack>
+        <Heading fontSize="7xl" as="h1">The Dice Game</Heading>
+        <Button
+          alignSelf="flex-end"
+          bg={"black"}
+          color={"white"}
+          _hover={{ bg: "green", color: 'yellow' }}
+        >
+          Play Now
+        </Button>
+      </Stack>
+    </Flex>
   );
 }
 
