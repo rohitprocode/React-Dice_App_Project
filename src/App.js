dc7071ef52e3b4afa357ec636ef3b3bf6d1a7876
rohitprocode <rohit990797@gmail.com>
2023-09-21 18:41:30 +0530

    Dice images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import {Box} from "@chakra-ui/layout"
+import {Flex} from "@chakra-ui/layout"
+import { Image } from "@chakra-ui/react";
 
 const App = () =>{
-  return <Box color="Red" backgroundColor="blue" > This is Box</Box>
+  return (<Flex>
+    <Image src="/dices.png"/>
+  </Flex>
+  );
 }
 
 export default App;
